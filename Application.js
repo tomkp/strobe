a7b54ba6b0c2474f7d800a267940ede133e6d821
tomkp <tom@tomkp.com>
2015-03-14 15:00:23 +0000

    package tweaking

--- Application.js
+++ Application.js
@@ -1,1 +1,15 @@
 var Calendar = require('react-calendar-pane');
+var React = require('react');
+
+
+var Example = React.createClass({
+
+    render: function() {
+        return (
+            <Calendar />
+        );
+    }
+
+});
+
+React.render(<Example />, document.body);
